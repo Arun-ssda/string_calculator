4823d3638bf2bf14bc0b6f86f0f3af7306ba74a0
Arun-ssda <arunssda98@gmail.com>
2025-05-19 14:16:10 +0530

    [Step 2] Removed guard to check number count

--- app/models/calculator.rb
+++ app/models/calculator.rb
@@ -2,7 +2,7 @@ class Calculator
   def self.add(numbers)
     raise ArgumentError, "input should be a string" unless numbers.is_a?(String)
     numbers_array = numbers.split(",")
-    raise ArgumentError, "upto 2 numbers allowed" unless numbers_array.size <= 2
+    # raise ArgumentError, "upto 2 numbers allowed" unless numbers_array.size <= 2
     numbers_array.each do |number|
       raise ArgumentError, "Not a number" unless number.to_i.to_s == number
     end
