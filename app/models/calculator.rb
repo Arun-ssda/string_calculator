0d3750ae53209541135d4318a36201c6856102ab
Arun-ssda <arunssda98@gmail.com>
2025-05-19 14:11:47 +0530

    [Step-1] The method should return the sum

--- app/models/calculator.rb
+++ app/models/calculator.rb
@@ -3,5 +3,9 @@ class Calculator
     raise ArgumentError, "input should be a string" unless numbers.is_a?(String)
     numbers_array = numbers.split(",")
     raise ArgumentError, "upto 2 numbers allowed" unless numbers_array.size <= 2
+    numbers_array.each do |number|
+      raise ArgumentError, "Not a number" unless number.to_i.to_s == number
+    end
+    numbers_array.sum(&:to_i)
   end
 end
