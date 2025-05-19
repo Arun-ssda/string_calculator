2a960d5597e7c01f2955026ccfcc9058db59c111
Arun-ssda <arunssda98@gmail.com>
2025-05-19 14:46:30 +0530

    [Step-4] Changes to support custom delimiter

--- app/models/calculator.rb
+++ app/models/calculator.rb
@@ -1,7 +1,13 @@
 class Calculator
   def self.add(numbers)
     raise ArgumentError, "input should be a string" unless numbers.is_a?(String)
-    numbers_array = numbers.split(/[\,\n]/)
+    if numbers.match?(/\/\/.\n/)
+      delimiter = numbers.match(/\/\/(.)\n/)[1]
+      numbers = numbers.split("\n", 2).last
+    else
+      delimiter = ","
+    end
+    numbers_array = numbers.split(/[#{delimiter}\n]/)
     # raise ArgumentError, "upto 2 numbers allowed" unless numbers_array.size <= 2
     numbers_array.each do |number|
       raise ArgumentError, "Not a number" unless number.to_i.to_s == number
