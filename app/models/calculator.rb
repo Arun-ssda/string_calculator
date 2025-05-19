116d5aaf8ec9a41e1535dc36149dcf04a4f9558c
Arun-ssda <arunssda98@gmail.com>
2025-05-19 14:54:34 +0530

    [Step 5] Code to raise for negative numbers

--- app/models/calculator.rb
+++ app/models/calculator.rb
@@ -1,14 +1,24 @@
 class Calculator
+  def self.get_delimiter(numbers)
+    numbers.match?(/\/\/.\n/) ? numbers.match(/\/\/(.)\n/)[1] : ","
+  end
+
+  def self.get_numbers(numbers)
+    numbers.match?(/\/\/.\n/) ? numbers.split("\n", 2).last : numbers
+  end
+
+  def self.check_for_negatives!(numbers_array)
+    negatives = numbers_array.select { |number| number.to_i < 0 }
+    raise ArgumentError, "negatives not allowed: #{negatives.join(', ')}" unless negatives.empty?
+  end
+
   def self.add(numbers)
     raise ArgumentError, "input should be a string" unless numbers.is_a?(String)
-    if numbers.match?(/\/\/.\n/)
-      delimiter = numbers.match(/\/\/(.)\n/)[1]
-      numbers = numbers.split("\n", 2).last
-    else
-      delimiter = ","
-    end
+    delimiter = get_delimiter(numbers)
+    numbers = get_numbers(numbers)
     numbers_array = numbers.split(/[#{delimiter}\n]/)
     # raise ArgumentError, "upto 2 numbers allowed" unless numbers_array.size <= 2
+    check_for_negatives!(numbers_array)
     numbers_array.each do |number|
       raise ArgumentError, "Not a number" unless number.to_i.to_s == number
     end
