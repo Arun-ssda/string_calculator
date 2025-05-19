1795a769351f965b9dff73780ffb96fff20cd659
Arun-ssda <arunssda98@gmail.com>
2025-05-19 14:23:07 +0530

    [Step-3] Support \n as delimiter on code

--- app/models/calculator.rb
+++ app/models/calculator.rb
@@ -1,7 +1,7 @@
 class Calculator
   def self.add(numbers)
     raise ArgumentError, "input should be a string" unless numbers.is_a?(String)
-    numbers_array = numbers.split(",")
+    numbers_array = numbers.split(/[\,\n]/)
     # raise ArgumentError, "upto 2 numbers allowed" unless numbers_array.size <= 2
     numbers_array.each do |number|
       raise ArgumentError, "Not a number" unless number.to_i.to_s == number
